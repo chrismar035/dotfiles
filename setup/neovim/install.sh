1b052f99854eb6237cb3a7121f262d90405331c5
Chris Marshall <chris@codegoalie.com>
2020-03-28 11:29:40 +0000

    Install neovim from ppa
    Also add how to get appimage version as backup

--- setup/neovim/install.sh
+++ setup/neovim/install.sh
@@ -2,6 +2,12 @@
 
 set -e
 
-sudo apt install neovim python-neovim python3-neovim python3-pip
+VERSION=0.4.3
+
+# wget https://github.com/neovim/neovim/releases/download/v$VERSION/nvim.appimage -o ~/bin/nvim
+# chmod u+x ~/bin/nvim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install -y neovim python-pip python3-pip
 pip install pynvim
 pip3 install pynvim
