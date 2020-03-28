d6324e0941e87f07892ab572d58d7434cdc280a9
Chris Marshall <chris@codegoalie.com>
2020-03-28 11:29:19 +0000

    Fix git config paths and install diff-so-fancy

--- setup/git/configure.sh
+++ setup/git/configure.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-ln -s ~/.local/dotfiles/templates/.gitconfig ~/.gitconfig
-ln -s ~/.local/dotfiles/templates/.gitignore ~/.gitignore
+wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy \
+  -O ~/bin/diff-so-fancy
+chmod +x ~/bin/diff-so-fancy
+
+ln -s ~/.local/dotfiles/templates/git/.gitconfig ~/.gitconfig
+ln -s ~/.local/dotfiles/templates/git/.gitignore ~/.gitignore
