71d7385882c5b573eca2175bf164027f58353e05
Chris Marshall <chris@codegoalie.com>
2020-03-28 11:30:13 +0000

    Backup old zshrc

--- setup/oh-my-zsh/configure.sh
+++ setup/oh-my-zsh/configure.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+if [ -f ~/.zshrc ]; then
+  echo ".zshrc exists. Moving to ~/.zshrc.backup"
+  mv ~/.zshrc  ~/.zshrc.backup
+fi
 ln -s ~/.local/dotfiles/templates/oh-my-zsh/.zshrc ~/.zshrc
