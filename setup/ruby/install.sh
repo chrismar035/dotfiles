161de41a6a95a707793347b0d702458dda9878df
Chris Marshall <chris@codegoalie.com>
2020-04-08 20:23:51 +0000

    Install ruby (rbenv) with homebrew

--- setup/ruby/install.sh
+++ setup/ruby/install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+RUBY_VERSION=2.7.1
+
 # git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 
 # echo "Trying to compile dynamic bash extension for rbenv. It's ok of this fails."
@@ -9,5 +11,10 @@ set -e
 
 # ~/.rbenv/bin/rbenv init
 
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+# mkdir -p "$(rbenv root)"/plugins
+# git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+brew install rbenv
+rbenv install $RUBY_VERSION
+# If the above fails with a complaint about openssl, try this:
+# CONFIGURE_OPTS=--with-openssl-dir=`brew --prefix openssl` rbenv install $RUBY_VERSION
