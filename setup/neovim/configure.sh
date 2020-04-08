a46612ab6f45153fa61cea44f3215b13d2d22ba2
Chris Marshall <chris@codegoalie.com>
2020-04-08 20:47:04 +0000

    Install node and ruby neovim supports

--- setup/neovim/configure.sh
+++ setup/neovim/configure.sh
@@ -8,3 +8,5 @@ ln -s ~/.local/dotfiles/templates/neovim ~/.config/nvim
 vim -c 'PlugInstall|q'
 vim -c 'CocInstall -sync coc-json coc-html coc-css coc-cssmodules coc-eslint coc-go coc-markdownlint coc-yank|q'
 
+gem install neovim
+npm install -g neovim
