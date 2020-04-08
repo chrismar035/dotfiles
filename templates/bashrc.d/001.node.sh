753dfeb778e84c55813d1846b9613ff856761edf
Chris Marshall <chris@codegoalie.com>
2020-04-08 20:40:47 +0000

    Update node bashrc to account for homebrew

--- templates/bashrc.d/001.node.sh
+++ templates/bashrc.d/001.node.sh
@@ -1,1 +1,6 @@
-. ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$HOME/.nvm/nvm.sh" ] && . "$HOME/.nvm/nvm.sh"  # This loads nvm from your home dir
+
+[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm from homebrew
+[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
