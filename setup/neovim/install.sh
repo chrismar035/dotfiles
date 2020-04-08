dc2431525579ec984801d1dba6f228f844ce140b
Chris Marshall <chris@codegoalie.com>
2020-04-08 20:23:34 +0000

    Install neovim with homebrew

--- setup/neovim/install.sh
+++ setup/neovim/install.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-VERSION=0.4.3
-
+# VERSION=0.4.3
 # wget https://github.com/neovim/neovim/releases/download/v$VERSION/nvim.appimage -o ~/bin/nvim
 # chmod u+x ~/bin/nvim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install -y neovim python-pip python3-pip
+# sudo add-apt-repository ppa:neovim-ppa/stable
+# sudo apt update
+
+brew install neovim
+sudo apt install -y \
+  # neovim \
+  python-pip python3-pip
 pip install pynvim
 pip3 install pynvim
