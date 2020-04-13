b98f6b4a6aec5baaf6ec849c57b68a8f2eb77ad2
Chris Marshall <chris@codegoalie.com>
2020-04-13 16:21:12 -0400

    Setup docker non-root access and start on boot

--- setup/docker/install.sh
+++ setup/docker/install.sh
@@ -31,6 +31,12 @@ sudo apt-get update
 sudo apt-get install -y \
   docker-ce docker-ce-cli containerd.io
 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo systemctl enable docker
+
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
