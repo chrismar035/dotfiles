84af5d5beafc02b1020e31d5254147f6e6a4d0c0
Chris Marshall <chris@codegoalie.com>
2020-04-13 10:48:17 -0400

    Use homebrew's OpenSSL for Ruby

--- templates/bashrc.d/001.ruby.sh
+++ templates/bashrc.d/001.ruby.sh
@@ -3,3 +3,7 @@
 
 export PATH="$PATH:$HOME/.rbenv/bin"
 eval "$(rbenv init -)"
+
+# Use Homebrew's OpenSSL version; which is updated
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
