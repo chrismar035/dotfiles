2d2c50a8024b73d8b08bc271bd352205d79b9810
Chris Marshall <chris@codegoalie.com>
2020-04-20 20:08:07 -0400

    Use ripgrep istead of silversearcher

--- setup/shell/install.sh
+++ setup/shell/install.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-sudo apt install -y silversearcher-ag
+# sudo apt install -y silversearcher-ag
+brew install ripgrep
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
