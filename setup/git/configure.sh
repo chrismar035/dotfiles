d18d509df958f30d0ffb2151b56d3e7f0489663b
Chris Marshall <chris@codegoalie.com>
2020-04-29 07:22:37 -0400

    Use delta for git diffs
    https://github.com/dandavison/delta

--- setup/git/configure.sh
+++ setup/git/configure.sh
@@ -3,9 +3,10 @@
 set -e
 
 mkdir -p ~/bin
-wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy \
-  -O ~/bin/diff-so-fancy
-chmod +x ~/bin/diff-so-fancy
+# wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy \
+#   -O ~/bin/diff-so-fancy
+# chmod +x ~/bin/diff-so-fancy
+ln -s ~/.local/dotfiles/bin/delta-0.1.1 ~/bin/delta
 
 ln -s ~/.local/dotfiles/templates/git/.gitconfig ~/.gitconfig
 ln -s ~/.local/dotfiles/templates/git/.gitignore ~/.gitignore
