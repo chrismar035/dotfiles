1686fb7759f558ba6bb389fb0f6514094bde8cf6
Chris Marshall <chris@codegoalie.com>
2020-05-13 10:45:01 -0400

    Install foliate for eBook reading

---
+++ setup/foliate/install.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+set -e
+
+flatpak install flathub com.github.johnfactotum.Foliate
+
+# Run with. It should add to the app menu after first run (or waitting)
+# flatpak run com.github.johnfactotum.Foliate
