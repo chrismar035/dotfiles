c8724922de2afd246e5af53eaa4b07acc755d0d6
Chris Marshall <chris@codegoalie.com>
2020-05-13 10:44:51 -0400

    Install and configure i3

---
+++ setup/i3/configure.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+set -e
+
+mkdir -p ~/.config/i3
+ln -s ~/.local/dotfiles/templates/i3-config ~/.config/i3/config
---
+++ setup/i3/install.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+set -e
+
+sudo apt install -y i3
+echo "i3 window manager installed. Log out and log in to use."
