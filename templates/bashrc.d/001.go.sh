fcc64a000b2727f47f554ad94c8aff631fa8c017
Chris Marshall <chris@codegoalie.com>
2020-05-22 11:13:58 -0400

    Explicitly set GOPATH
    This is necessary for the new modvendor method from:
    https://github.com/go-modules-by-example/index/blob/master/012_modvendor/README.md

--- templates/bashrc.d/001.go.sh
+++ templates/bashrc.d/001.go.sh
@@ -2,5 +2,6 @@
 
 export PATH="$PATH:/usr/local/go/bin"
 export GOPRIVATE=github.com/growthtools/*,github.com/codegoalie/disneyresorts
+export GOPATH=~/go
 
 alias gmtv='go mod tidy && go mod vendor'
