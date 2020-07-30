cbd133a17a3dcc785f21882f5b60cd15a2cdc93f
Chris Marshall <chris@codegoalie.com>
2020-07-30 13:36:45 -0400

    Go mod vendor or modvendor

--- templates/bashrc.d/001.go.sh
+++ templates/bashrc.d/001.go.sh
@@ -4,5 +4,15 @@ export PATH="$PATH:/usr/local/go/bin"
 export GOPRIVATE=github.com/growthtools/*,github.com/codegoalie/disneyresorts
 export GOPATH=~/go
 
-alias gmtv='go mod tidy && go mod vendor'
 alias gmt='go mod tidy'
+
+# alias gmtv='go mod tidy && go mod vendor'
+gmtv() {
+  go mod tidy
+  if [ -f vendor.sh ]; then
+    echo "running ./vendor.sh"
+    ./vendor.sh
+  else
+    go mod vendor
+  fi
+}
