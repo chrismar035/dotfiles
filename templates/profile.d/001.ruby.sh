3ab100af012733ed767bc9cdb131f04e04afd3fb
Chris Marshall <chris@codegoalie.com>
2021-01-27 13:36:17 -0500

    Add some nice ruby aliases

--- templates/profile.d/001.ruby.sh
+++ templates/profile.d/001.ruby.sh
@@ -8,3 +8,7 @@ eval "$(rbenv init -)"
 # export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 # without brew --prefix
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/home/linuxbrew/.linuxbrew/opt/openssl@1.1"
+
+alias be='bundle exec'
+alias brake='bundle exec rake'
+alias brails='bundle exec rails'
