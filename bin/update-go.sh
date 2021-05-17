fdcb2c2bab4edd22e5e8d7b5ef304fddc8529afe
Chris Marshall <chris@codegoalie.com>
2021-05-17 09:23:19 -0400

    Track update go script

---
+++ bin/update-go.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]
+then
+  echo "Please provide a version number, like 1.13.5"
+  exit 1
+fi
+
+FILENAME=go$1.linux-amd64.tar.gz
+URL=https://dl.google.com/go/$FILENAME
+
+wget $URL
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf $FILENAME
+rm $FILENAME
+go version
---
+++ setup/bin/configure.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+set -e
+
+ln -s $HOME/.local/dotfiles/bin/update-go.sh $HOME/bin/update-go.sh
