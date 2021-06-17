c219ac0d0ed99cd8c99e55f76ad26f85d3de099f
Chris Marshall <chris@codegoalie.com>
2021-06-17 08:44:53 -0400

    Speed up terminal start

--- templates/bashrc.d/001.node.sh
+++ templates/bashrc.d/001.node.sh
@@ -1,2 +1,3 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$HOME/.nvm/nvm.sh" ] && . "$HOME/.nvm/nvm.sh"  # This loads nvm from your home dir
+[ -s "$HOME/.nvm/nvm.sh" ] && . "$HOME/.nvm/nvm.sh" --no-use  # This loads nvm from your home dir
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
