47cd705584b77b031045cbf4668363c1819c9277
Chris Marshall <chris@codegoalie.com>
2021-06-17 08:52:26 -0400

    Create gifs from videos
    src: https://www.ehfeng.com/gif-screen-recordings-on-macos/

---
+++ templates/profile.d/333.mov2gif.sh
@@ -0,0 +1,7 @@
+mov2gif () {
+	ffmpeg -i $1 -vf "fps=10,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 screen.gif
+}
+
+mov2gif720p () {
+	ffmpeg -i $1 -vf "fps=10,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 screen.gif
+}
