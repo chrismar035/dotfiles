381c252018ffd31a35bf26af923d5f602d65bd67
Chris Marshall <chris@codegoalie.com>
2021-07-06 06:58:03 -0400

    Convert videos to webp animations too!

--- templates/profile.d/333.mov2gif.sh
+++ templates/profile.d/333.mov2gif.sh
@@ -5,3 +5,11 @@ mov2gif () {
 mov2gif720p () {
 	ffmpeg -i $1 -vf "fps=10,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 screen.gif
 }
+
+mov2webp () {
+  ffmpeg -i $1 \
+    -vf "fps=10,scale=720:-1:flags=lanczos" \
+    -vcodec libwebp -lossless 0 -compression_level 6 \
+    -q:v 50 -loop 0 \
+    -preset picture -an -vsync 0 screen.webp
+}
